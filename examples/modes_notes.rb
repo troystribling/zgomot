5364c0721ebca2d7e883d53bcb25c1d8b44491d4
Troy Stribling <troy.stribling@gmail.com>
2013-07-06 14:10:25 -0700

    have not looked at code in a while. it needed a clean up

--- examples/modes_notes.rb
+++ examples/modes_notes.rb
@@ -1,16 +1,13 @@
 require 'rubygems'
 require "#{File.dirname(__FILE__)}/../lib/zgomot"
 
-#.........................................................................................................
 before_start do
   Zgomot.logger.level = Logger::DEBUG
 end
 
-#.........................................................................................................
 str 'modes', [np([:A,4],nil,:l=>4), np([:A,4],nil,:l=>4).reverse!.shift, n(:R)], :lim=>6 do |pattern|
   Zgomot.logger.info "TONIC: [A,4], MODE: #{count-1}"
   ch << pattern.mode!(count)
 end
 
-#.........................................................................................................
 play
--- examples/scales_notes.rb
+++ examples/scales_notes.rb
@@ -1,19 +1,15 @@
 require 'rubygems'
 require "#{File.dirname(__FILE__)}/../lib/zgomot"
 
-#.........................................................................................................
 before_start do
   Zgomot.logger.level = Logger::DEBUG
 end
 
-#.........................................................................................................
 pclass = [:C, :Cs, :D, :Ds, :E, :F, :Fs, :G, :Gs, :A, :As, :B]
 
-#.........................................................................................................
 str 'scale', [np(nil,:dorian,:l=>4), np(nil,:dorian,:l=>4).reverse!.shift, n(:R)], :lim=>pclass.length do |pattern|
   Zgomot.logger.info "TONIC: [#{pclass[count-1]},4], MODE: dorian"
   ch << pattern.tonic!([pclass[count-1], 4])
 end
 
-#.........................................................................................................
 play
