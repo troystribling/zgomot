9c9a2d5e5f46e9ca8cef9737f4b81a190cfc94a8
Troy Stribling <troy.stribling@gmail.com>
2013-07-14 20:25:05 -0700

    strating CC processing

--- examples/simple_input.rb
+++ examples/simple_input.rb
@@ -5,12 +5,11 @@ before_start do
   Zgomot.logger.level = Logger::DEBUG
 end
 
-str 'notes' do
-  ch << [n([:C,5]), n(:B), n(:R), n(:G), n(:C,:l=>2), n([:E,5],:l=>2)]
-end
-
 if sources.include?("nanoKONTROL")
   add_input("nanoKONTROL")
+  str 'notes' do
+    ch << [n([:C,5]), n(:B), n(:R), n(:G), n(:C,:l=>2), n([:E,5],:l=>2)]
+  end
 end
 
 play
