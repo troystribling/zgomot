d2fa7d49c9db136d9a055cdbeac666e1a4ed00ba
Troy Stribling <troy.stribling@gmail.com>
2013-07-17 20:52:04 -0700

    method delegation for progressions really works now

--- examples/prog_chords.rb
+++ examples/prog_chords.rb
@@ -5,7 +5,7 @@ before_start do
   Zgomot.logger.level = Logger::DEBUG
 end
 
-str 'prog', cp([:B,3],:ionian,:l=>4)[1,4,5,5], :lim=>1 do |pattern|
+str 'prog', cp([:B,3],:ionian,:l=>4)[1,4,5,5], :lim=>6 do |pattern|
   ch << pattern
 end
 
--- examples/simple_input.rb
+++ examples/simple_input.rb
@@ -8,10 +8,13 @@ end
 if sources.include?("nanoKONTROL")
   add_input("nanoKONTROL")
   add_cc(:mode, 17, :type => :cont, :min => 0, :max => 6, :init => 0)
+  add_cc(:reverse, 13, :type => :switch)
   str 'simple_input', np([:A,4],2,:l=>4)[7,5,3,1] do |pattern|
-    mode = cc(:mode).to_i
-    puts mode
-    ch << pattern.mode!(mode)
+    ch << if cc(:reverse)
+            pattern.mode!(cc(:mode).to_i).reverse!
+          else
+            pattern.mode!(cc(:mode).to_i)
+          end
   end
 end
 
