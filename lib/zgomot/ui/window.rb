1725fe5c4319d47d73d798fcd62c7965f9500a0c
Troy Stribling <troy.stribling@gmail.com>
2013-07-23 23:31:32 -0700

    getting started wit ncurses

--- lib/zgomot/ui/window.rb
+++ lib/zgomot/ui/window.rb
@@ -1,19 +1,47 @@
 module Zgomot::UI
+  WIDTH = 80
   class Window
     class << self
       def dash
+        Curses.noecho
         Curses.init_screen
-        my_str = "ZGOMOT"
-        win = Curses::Window.new(8, (my_str.length + 10), (Curses.lines - 8)/2, (Curses.cols - (my_str.length + 10))/2)
-        win.box(?|, ?-)
-        win.setpos(2,3)
-        win.addstr(my_str)
-        win.refresh
-        win.getch
-        win.close
-        Curses.close_screen
+        win = Curses::Window.new(0, WIDTH, 0, 0)
+        loop do
+          case win.getch
+          #when Curses::Key::UP then ttt.move(0,-1)
+          #when Curses::Key::DOWN then ttt.move(0,1)
+          when ?q
+            win.close
+            Curses.close_screen
+          end
+        end
       end
     end
+  end
+  class Globals
+    HEIGHT = 10
+    class << self
+      def window
+      end
+    end
+  end
+  class Str
+    HEIGHT = (Curses.lines - Globals::HEIGHT)/2
+    TOP = Globals::HEIGHT
+    class << self
+      def window
+      end
+    end
+  end
+  class CC
+    HEIGHT = (Curses.lines - Globals::HEIGHT)/2
+    TOP = Str::HEIGHT + Globals::HEIGHT
+    class << self
+      def window
+      end
+    end
+  end
+  class Text
   end
 end
 
