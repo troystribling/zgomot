37d0a8aa7eb03d3e56fb8249f8b495d33629fd61
Troy Stribling <troy.stribling@gmail.com>
2013-07-26 08:29:21 -0700

    more ui work

--- lib/zgomot/ui/window.rb
+++ lib/zgomot/ui/window.rb
@@ -1,5 +1,12 @@
 module Zgomot::UI
   WIDTH = 80
+  COLOR_GREEN = Curses::COLOR_GREEN
+  COLOR_BLACK = Curses::COLOR_BLACK
+  COLOR_WHITE = Curses::COLOR_WHITE
+  COLOR_YELLOW = Curses::COLOR_YELLOW
+  COLOR_CYAN = Curses::COLOR_CYAN
+  COLOR_RED = Curses::COLOR_RED
+  COLOR_MAGENTA = Curses::COLOR_MAGENTA
   class Window
     class << self
       def init_curses
@@ -7,12 +14,12 @@ module Zgomot::UI
         Curses.init_screen
         Curses.start_color
         Curses.curs_set(0)
-        Curses.init_pair(Curses::COLOR_GREEN,Curses::COLOR_GREEN,Curses::COLOR_BLACK)
-        Curses.init_pair(Curses::COLOR_WHITE,Curses::COLOR_WHITE,Curses::COLOR_BLACK)
-        Curses.init_pair(Curses::COLOR_YELLOW,Curses::COLOR_YELLOW,Curses::COLOR_BLACK)
-        Curses.init_pair(Curses::COLOR_CYAN,Curses::COLOR_CYAN,Curses::COLOR_BLACK)
-        Curses.init_pair(Curses::COLOR_RED,Curses::COLOR_RED,Curses::COLOR_BLACK)
-        Curses.init_pair(Curses::COLOR_MAGENTA,Curses::COLOR_MAGENTA,Curses::COLOR_BLACK)
+        Curses.init_pair(COLOR_GREEN,COLOR_GREEN,COLOR_BLACK)
+        Curses.init_pair(COLOR_WHITE,COLOR_WHITE,COLOR_BLACK)
+        Curses.init_pair(COLOR_YELLOW,COLOR_YELLOW,COLOR_BLACK)
+        Curses.init_pair(COLOR_CYAN,COLOR_CYAN,COLOR_BLACK)
+        Curses.init_pair(COLOR_RED,COLOR_RED,COLOR_BLACK)
+        Curses.init_pair(COLOR_MAGENTA,COLOR_MAGENTA,COLOR_BLACK)
       end
       def dash
         init_curses
@@ -26,6 +33,7 @@ module Zgomot::UI
           when ?q
             win.close
             Curses.close_screen
+            break
           end
         end
       end
@@ -36,7 +44,7 @@ module Zgomot::UI
     attr_reader :window
     def initialize(parent_window)
       @window = parent_window.subwin(HEIGHT, WIDTH, 0, 0) 
-      TextWithValue.new(window, 'TESTING', 'VALUE', Curses::COLOR_GREEN, 20, 0, 0)
+      TextWithValue.new(window, 'TESTING', 'VALUE', COLOR_GREEN, 20, 0, 0)
       window.refresh
     end
   end
@@ -55,34 +63,42 @@ module Zgomot::UI
     end
   end
   class TextWithValue
-    attr_reader :text, :value, :window
+    attr_reader :text, :value, :window, :color
     def initialize(parent_window, text, value, color, width, top, left)
+      @color = color
       @window = parent_window.subwin(1, width, top, left)
       @window.attron(Curses.color_pair(color)|Curses::A_NORMAL) {
         @window << "#{text}: #{value}"
       }
     end
-    def value=(value)
-      @window.clear
-      @window.attron(Curses.color_pair(color)|Curses::A_NORMAL) {
-        @window << "#{text}: #{value}"
-      }
-    end
+    private
+      def value=(value)
+        @window.clear
+        @window.attron(Curses.color_pair(color)|Curses::A_NORMAL) {
+          @window << "#{text}: #{value}"
+        }
+      end
   end
   class TextRow
-    attr_accessor :windows
+    attr_reader :windows, :color
     def initialize(parent_window, values, color, top)
+      @color = color
       @windows = values.reduce([]) do|ws, v|
                    w = parent_window.subwin(1, WIDTH, top, 0)
                    w.attron(Curses.color_pair(color)|Curses::A_NORMAL) {w << v}
                    ws << w
                  end
     end
-    def row=(values)
-    end
+    private
+      def row=(values)
+        @windows.each do |w|
+          w.clear
+          w.attron(Curses.color_pair(color)|Curses::A_NORMAL) {w << v.shift}
+        end
+      end
   end
   class Title
-    def initialize(title)
+    def initialize(parent_window, title)
 
     end
   end
