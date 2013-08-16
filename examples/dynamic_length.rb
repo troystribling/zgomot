b6ffd28c149069ccec1135fccf93a3712f2b93e6
Troy Stribling <troy.stribling@gmail.com>
2013-08-15 23:30:53 -0700

    fixed bug in length and velocity transforms

---
+++ examples/dynamic_length.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require "#{File.dirname(__FILE__)}/../lib/zgomot"
+
+before_start do
+  Zgomot.logger.level = Logger::DEBUG
+end
+
+if sources.include?("nanoKONTROL")
+  add_input("nanoKONTROL")
+  add_cc(:mult, 17, :type => :cont, :min => 1, :max => 3, :init => 0)
+  len = 4
+  str 'simple_input', np([:A,4],2,:l=>len)[7,5,3,1] do |pattern|
+    puts (2**cc(:mult).to_i)*len
+    pattern.length = (2**cc(:mult).to_i)*len
+    pattern
+  end
+  play
+end
+
+
