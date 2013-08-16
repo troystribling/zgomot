ee40fb652c915cbdaf70c32f6f31b1c160a915cb
Troy Stribling <troy.stribling@gmail.com>
2013-08-16 08:29:10 -0700

    fixed type

--- examples/dynnamic_length_perc.rb
+++ examples/dynnamic_length_perc.rb
@@ -9,7 +9,7 @@ if sources.include?("nanoKONTROL")
   add_input("nanoKONTROL")
   add_cc(:mult, 17, :type => :cont, :min => 0, :max => 4, :init => 0)
   len = 4
-  str 'perce', [pr(:low_floor_tom, :l => len),
+  str 'perc', [pr(:low_floor_tom, :l => len),
                  pr(:low_tom, :l => len),
                  n(:R, :l=> len),
                  pr(:high_mid_tom, :l => len)], :ch=>0 do |pattern|
